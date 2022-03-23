6b381f85b8da012aad3ac461bff00cd1ead59633
+ <+>
2022-03-23 11:07:59 -0400

    add javascript to render current date

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+    const dateDisplay = document.getElementById("date")
 
+    const displayDate = () => {
+        const today = new Date
+        const month = monthNames[today.getMonth()]
+        const day = today.getDate()
+        dateDisplay.innerText = `${month} ${day}`
+    }
+    displayDate()
 })
